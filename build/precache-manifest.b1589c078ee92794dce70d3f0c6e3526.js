b562b92e987e2f4e483f9c6b342da58560f0f8a6
Anton Nesterov <arch.nesterov@gmail.com>
2019-11-08 14:23:35 +0300

    [build]

---
+++ build/precache-manifest.b1589c078ee92794dce70d3f0c6e3526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803d1ed959c54d4f91fbd3f13c305304",
+    "url": "/YetYetReact/build/index.html"
+  },
+  {
+    "revision": "2a3957324189a3168104",
+    "url": "/YetYetReact/build/static/css/main.31d944ea.chunk.css"
+  },
+  {
+    "revision": "51c22cc3f5fce40ce977",
+    "url": "/YetYetReact/build/static/js/2.2dc49339.chunk.js"
+  },
+  {
+    "revision": "2a3957324189a3168104",
+    "url": "/YetYetReact/build/static/js/main.8921097b.chunk.js"
+  },
+  {
+    "revision": "e8b3a290963d57dd1a2c",
+    "url": "/YetYetReact/build/static/js/runtime-main.802fb759.js"
+  },
+  {
+    "revision": "6d5e7efad4d812922fd996225eb1f926",
+    "url": "/YetYetReact/build/static/media/wood-pattern.6d5e7efa.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d56be199efcf982b05ace3243028c01f.js"
+  "/YetYetReact/build/precache-manifest.b1589c078ee92794dce70d3f0c6e3526.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YetYetReact/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
